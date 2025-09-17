1e0daf500781baa5d1f250ef5b560ed667076c1b
Brent Crowley <brent.crowley@seventhbeam.com>
2025-09-17 19:42:01 +1000

    feat(commands): tidy up game commands

--- Shared/Sources/Domain/Models/Game/Commands/GameCommands.swift
+++ Shared/Sources/Domain/Models/Game/Commands/GameCommands.swift
@@ -1,6 +1,8 @@
 import Foundation
 
 extension ThrowingCommand where S == Game {
+    static func throwingGameCommand(_ command: ThrowingCommand<Game>) -> Self { command }
+
     public static func pickUpFromDrawPile(player: Player.Up) -> Self {
         .init { game in
             try ThrowingCommand<Deck>.drawPilePickUp(player: player).execute(on: &game.deck)
@@ -11,6 +13,8 @@ extension ThrowingCommand where S == Game {
 
 // MARK: - Command
 extension Command where S == Game {
+    static func gameCommand(_ command: Command<Game>) -> Self { command }
+
     public static func discardToLeftPile(cardID: Card.ID) -> Self { discardCard(id: cardID, to: .discardLeft) }
     public static func discardToRightPile(cardID: Card.ID) -> Self { discardCard(id: cardID, to: .discardRight) }
 }
@@ -18,7 +22,10 @@ extension Command where S == Game {
 // MARK: - Private Static API
 extension Command where S == Game {
     private static func discardCard(id: Card.ID, to pile: Deck.Pile) -> Self {
-        .init { $0.deck.update(cardID: id, toLocation: .pile(pile)) }
+        .init {
+            $0.deck.update(cardID: id, toLocation: .pile(pile))
+            gameCommand(.changePhase(to: .waitingForPlay)).execute(on: &$0)
+        }
     }
 }
 
