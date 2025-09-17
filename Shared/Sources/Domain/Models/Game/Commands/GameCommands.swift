ee533085742d2484bbbf3e377c4dda382e177eef
Brent Crowley <brent.crowley@seventhbeam.com>
2025-09-17 19:29:58 +1000

    feat(game-engine): add discard pile commands

--- Shared/Sources/Domain/Models/Game/Commands/GameCommands.swift
+++ Shared/Sources/Domain/Models/Game/Commands/GameCommands.swift
@@ -7,11 +7,18 @@ extension ThrowingCommand where S == Game {
             Command<Game>.changePhase(to: .waitingForDiscard).execute(on: &game)
         }
     }
+}
 
-    public static func player(_ player: Player.Up, discard card: Card.ID, to discardPile: Deck.Pile) -> Self {
-        .init { game in
-            try ThrowingCommand<Deck>.drawPilePickUp(player: player).execute(on: &game.deck)
-            Command<Game>.changePhase(to: .waitingForPlay).execute(on: &game)
-        }
+// MARK: - Command
+extension Command where S == Game {
+    public static func discardToLeftPile(cardID: Card.ID) -> Self { discardCard(id: cardID, to: .discardLeft) }
+    public static func discardToRightPile(cardID: Card.ID) -> Self { discardCard(id: cardID, to: .discardRight) }
+}
+
+// MARK: - Private Static API
+extension Command where S == Game {
+    private static func discardCard(id: Card.ID, to pile: Deck.Pile) -> Self {
+        .init { $0.deck.update(cardID: id, toLocation: .pile(pile)) }
     }
 }
+
--- Shared/Tests/DomainTests/ModelsTests/CommandTests/CommandGameTests.swift
+++ Shared/Tests/DomainTests/ModelsTests/CommandTests/CommandGameTests.swift
@@ -82,5 +82,33 @@ struct CommandGameTests {
         // THEN
         #expect(game.phase == phase)
     }
+
+    @Test("Success - Discard to left pile")
+    func successDiscardCardToLeftPile() throws {
+        // GIVEN
+        var game = Game.testMock()
+        let firstCard = try #require(game.deck.cards.first)
+        let command: Command<Game> = .discardToLeftPile(cardID: firstCard.id)
+
+        // WHEN
+        command.execute(on: &game)
+
+        // THEN
+        #expect(game.deck.cards.first?.location == .pile(.discardLeft))
+    }
+
+    @Test("Success - Discard to right pile")
+    func successDiscardCardToRightPile() throws {
+        // GIVEN
+        var game = Game.testMock()
+        let firstCard = try #require(game.deck.cards.first)
+        let command: Command<Game> = .discardToRightPile(cardID: firstCard.id)
+
+        // WHEN
+        command.execute(on: &game)
+
+        // THEN
+        #expect(game.deck.cards.first?.location == .pile(.discardRight))
+    }
 }
 
